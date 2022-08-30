5d71f3229e4b36d789d21d21e1636ad382bd81c1
ollie <ollie@ollierobinson.co.uk>
2022-08-31 00:20:03 +0100

    Updated nuget pushing package pipeline pscript

--- PublishNuget.sh
+++ PublishNuget.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+if [ $# -eq 3 ]
+then
+  rm Nuget/*
+  dotnet pack "$1" -c Release -o Nuget/ -p:PackageVersion="$2"
+  if [ $? -ne 0 ]
+  then
+    echo "Pack failed. Not pushing."
+    exit 0
+  fi
+  dotnet nuget push Nuget/"$1"."$2".nupkg -k "$3" -s https://api.nuget.org/v3/index.json
+  exit 0
+fi
+
 if [ $# -ne 2 ]
 then
-  echo "Required 2 arguments, <version number> <api key>"
+  echo "Required 2 arguments, <version number> <api key>, or 3 arguments, <package name> <version number> <api key>"
   exit 0
 fi
 
