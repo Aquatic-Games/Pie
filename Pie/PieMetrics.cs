86c04a823d28cfe72c2c35bc91b6cdbb0e46a35a
ollie <ollie@ollierobinson.co.uk>
2022-10-01 00:08:26 +0100

    Added version numbers

--- Pie/PieMetrics.cs
+++ Pie/PieMetrics.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Reflection;
+
 namespace Pie;
 
 public static class PieMetrics
 {
+    public static readonly Version Version = CalculateVersion();
+    
     /// <summary>
     /// The total number of vertex buffers currently active in the application.
     /// </summary>
@@ -26,4 +31,14 @@ public static class PieMetrics
     /// The total number of tris/polygons rendered in this frame.
     /// </summary>
     public static ulong TriCount;
+
+    private static Version CalculateVersion()
+    {
+        Version asmVer = Assembly.GetExecutingAssembly().GetName().Version;
+        DateTime dateTime = new DateTime(2022, 10, 1);
+        DateTime verTime = new DateTime(2000, 1, 1).AddDays(asmVer.Build);
+        TimeSpan ts = verTime - dateTime;
+        Version newVer = new Version(asmVer.Major, asmVer.Minor, ts.Days, asmVer.Revision);
+        return newVer;
+    }
 }
