123deaa3ccff25b218da7adfafa1c39c5ddf1e93
ollie <ollie@ollierobinson.co.uk>
2022-12-07 18:51:15 +0000

    removed datalength from texturedata

--- Pie/TextureData.cs
+++ Pie/TextureData.cs
@@ -6,45 +6,37 @@ public unsafe struct TextureData
 {
     public void* DataPtr;
 
-    public uint DataLength;
-
-    public TextureData(IntPtr data, uint dataLength)
+    public TextureData(IntPtr data)
     {
         DataPtr = data.ToPointer();
-        DataLength = dataLength;
     }
 
     public TextureData(void* data, uint dataLength)
     {
         DataPtr = data;
-        DataLength = dataLength;
     }
     
     public TextureData(byte[] data)
     {
         fixed (void* dat = data)
             DataPtr = dat;
-        DataLength = (uint) data.Length;
     }
 
     public TextureData(int[] data)
     {
         fixed (void* dat = data)
             DataPtr = dat;
-        DataLength = (uint) data.Length;
     }
 
     public TextureData(uint[] data)
     {
         fixed (void* dat = data)
             DataPtr = dat;
-        DataLength = (uint) data.Length;
     }
 
     public TextureData(float[] data)
     {
         fixed (void* dat = data)
             DataPtr = dat;
-        DataLength = (uint) data.Length;
     }
 }
