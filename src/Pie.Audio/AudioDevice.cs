d5a672a957e0ac5c8d3c35efa13c92e1efe17fa7
Skye <ollie@ollierobinson.co.uk>
2023-07-07 21:31:37 +0100

    audio device now protected against buffer underruns

--- src/Pie.Audio/AudioDevice.cs
+++ src/Pie.Audio/AudioDevice.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Pie.SDL;
 
 namespace Pie.Audio;
@@ -7,46 +9,88 @@ namespace Pie.Audio;
 /// <summary>
 /// Adds built-in audio playback to an <see cref="AudioSystem"/> using SDL.
 /// </summary>
-public class AudioDevice : AudioSystem
+public unsafe class AudioDevice : AudioSystem
 {
     private uint _device;
     private GCHandle _handle;
+
+    private float** _buffers;
+    private uint _periodSize;
+    private uint _numPeriods;
+    private uint _currentBuffer;
+
+    private object _lock;
     
     /// <summary>
     /// Create a new <see cref="AudioDevice"/> with the given sample rate and channels.
     /// </summary>
     /// <param name="sampleRate">The sample rate. Typical values include 44100 (CD quality) and 48000 (DAT quality).</param>
     /// <param name="channels">The number of channels. (Sounds that can be played at once).</param>
+    /// <param name="periodSize">The number of samples in each period.</param>
+    /// <param name="numPeriods">The number of periods.</param>
     /// <exception cref="Exception">Thrown if SDL fails to initialize.</exception>
-    public unsafe AudioDevice(uint sampleRate, ushort channels) : base(sampleRate, channels)
+    /// <remarks>Experiment with the <paramref name="periodSize"/> and the <paramref name="numPeriods"/> to get the best
+    /// result for your application. The larger the period size + number of periods, will result in less risk of a buffer
+    /// underrun, however will increase latency. Latency can be calculated (in seconds) using
+    /// ((periodSize * numPeriods) / sampleRate).</remarks>
+    public unsafe AudioDevice(uint sampleRate, ushort channels, uint periodSize = 768, uint numPeriods = 2) : base(sampleRate, channels)
     {
         if (Sdl.Init(Sdl.InitAudio) < 0)
             throw new Exception("SDL did not init: " + Sdl.GetErrorS());
 
+        _periodSize = periodSize;
+        _numPeriods = numPeriods;
+        
         SdlAudioSpec spec;
         spec.Freq = (int) sampleRate;
         spec.Format = Sdl.AudioF32;
         spec.Channels = 2;
-        spec.Samples = 512;
+        spec.Samples = (ushort) periodSize;
 
         Sdl.AudioCallback callback = new Sdl.AudioCallback(AudioCallback);
         _handle = GCHandle.Alloc(callback);
 
         spec.Callback = (delegate*<void*, byte*, int, void>) Marshal.GetFunctionPointerForDelegate(callback);
 
+        _buffers = (float**) NativeMemory.Alloc((nuint) (numPeriods * sizeof(float*)));
+        for (uint b = 0; b < numPeriods; b++)
+        {
+            _buffers[b] = (float*) NativeMemory.Alloc((nuint) (2 * periodSize * sizeof(float)));
+            ReadBufferStereoF32(_buffers[b], 2 * periodSize);
+        }
+
+        _lock = new object();
+
+        _currentBuffer = 0;
+
         _device = Sdl.OpenAudioDevice(null, 0, &spec, null, 0);
         Sdl.PauseAudioDevice(_device, 0);
     }
     
-    private unsafe void AudioCallback(void* arg0, byte* arg1, int arg2)
+    private void AudioCallback(void* arg0, byte* arg1, int arg2)
     {
-        ReadBufferStereoF32((float*) arg1, (nuint) arg2 / 4);
+        uint currBuffer = _currentBuffer++;
+        if (_currentBuffer >= _numPeriods)
+            _currentBuffer = 0;
+        
+        Unsafe.CopyBlock(arg1, _buffers[currBuffer], (uint) arg2);
+
+        Task.Run(() =>
+        {
+            lock (_lock)
+                ReadBufferStereoF32(_buffers[currBuffer], 2 * _periodSize);
+        });
     }
 
     public override void Dispose()
     {
         Sdl.CloseAudioDevice(_device);
         Sdl.QuitSubSystem(Sdl.InitAudio);
+        
+        for (uint b = 0; b < _numPeriods; b++)
+            NativeMemory.Free(_buffers[b]);
+        
+        NativeMemory.Free(_buffers);
         
         // Must call base.Dispose here as otherwise the AudioSystem will be disposed before SDL's audio device,
         // potentially causing issues.
