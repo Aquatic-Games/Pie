a91ac60539f124e9173ca5cf95b7ead624a17503
ollie <ollie@ollierobinson.co.uk>
2024-01-19 00:22:51 +0000

    add spirv + shaderc

--- scripts/BuildNative.sh
+++ scripts/BuildNative.sh
@@ -4,6 +4,7 @@ SPIRV_CROSS_NAME="SPIRV-Cross"
 SPIRV_CROSS_LIB_NAME="libspirv-cross-c-shared.so"
 
 SHADERC_NAME="shaderc"
+SHADERC_LIB_NAME="libshaderc_shared.so"
 
 if [ $# -ne 3 ]; then
   echo "Requires 3 parameters! <LIBS_LOCATION> <BUILD_LOCATION> <NATIVE_OUTPUT_LOCATION>"
@@ -16,10 +17,32 @@ native_output_location=$3
 
 current_loc=$(pwd)
 
+shaderc_location="${current_loc}/${libs_location}/${SHADERC_NAME}"
+shaderc_build_location="${current_loc}/${build_location}/${SHADERC_NAME}"
+
 spirv_location="${current_loc}/${libs_location}/${SPIRV_CROSS_NAME}"
 spirv_build_location="${current_loc}/${build_location}/${SPIRV_CROSS_NAME}"
 
 full_output_path="${current_loc}/${native_output_location}"
+
+echo "Creating ${SHADERC_NAME} build directory."
+mkdir -p "${shaderc_build_location}" || exit 1
+
+pushd "${shaderc_location}" || exit 1
+./utils/git-sync-deps || exit 1
+popd || exit 1
+
+pushd "${shaderc_build_location}" || exit 1
+
+echo "Creating ${SHADERC_NAME} build files."
+cmake "${shaderc_location}" -DCMAKE_BUILD_TYPE=Release -DSHADERC_SKIP_INSTALL=1 -DSHADERC_SKIP_TESTS=1 -DSHADERC_SKIP_EXAMPLES=1 -DSHADERC_SKIP_COPYRIGHT_CHECK=1 || exit 1
+
+echo "Building ${SHADERC_NAME}"
+cmake --build . --config Release || exit 1
+
+echo "${SHADERC_NAME} build successful!"
+
+popd || exit 1
 
 echo "Creating ${SPIRV_CROSS_NAME} build directory."
 mkdir -p "${spirv_build_location}" || exit 1
@@ -32,12 +55,17 @@ cmake "${spirv_location}" -DCMAKE_BUILD_TYPE=Release -DSPIRV_CROSS_SHARED=ON -DS
 echo "Building ${SPIRV_CROSS_NAME}."
 cmake --build . --config Release || exit 1
 
+echo "${SPIRV_CROSS_NAME} build successful!"
+
 popd || exit 1
 
 echo "Creating native output location."
 mkdir -p "${full_output_path}"
 
+echo "Copying ${SHADERC_LIB_NAME} to output location."
+find "${shaderc_build_location}" \( -iname "*.dll" -o -iname "*.so" \) -exec cp -L {} "${full_output_path}" \; || exit 1
+
 echo "Copying ${SPIRV_CROSS_LIB_NAME} to output location."
 find "${spirv_build_location}" \( -iname "*.dll" -o -iname "*.so" \) -exec cp -L {} "${full_output_path}" \; || exit 1
 
-echo "${SPIRV_CROSS_NAME} build successful!"
+echo "Successful."
--- src/Pie.Tests/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Pie;
-using Pie.Tests;
-using Pie.Tests.Tests;
-using Pie.Windowing;
-
-using TestBase tb = new AudioTest();
-tb.Run(GraphicsDevice.GetBestApiForPlatform());
