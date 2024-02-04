d78e8982a17eda0c63bb7abd174da3eeb4c6096f
ollie <ollie@ollierobinson.co.uk>
2024-02-04 17:02:29 +0000

    d3d sampler state done

--- src/Pie/Direct3D11/D3D11SamplerState.cs
+++ src/Pie/Direct3D11/D3D11SamplerState.cs
@@ -1,46 +1,59 @@
 using System;
-using Vortice.Direct3D11;
-using Vortice.Mathematics;
+using System.Diagnostics.CodeAnalysis;
+using TerraFX.Interop.DirectX;
+using static TerraFX.Interop.DirectX.D3D11_COMPARISON_FUNC;
+using static TerraFX.Interop.DirectX.D3D11_TEXTURE_ADDRESS_MODE;
+using static Pie.Direct3D11.DxUtils;
 
 namespace Pie.Direct3D11;
 
-internal sealed class D3D11SamplerState : SamplerState
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+internal sealed unsafe class D3D11SamplerState : SamplerState
 {
-    public ID3D11SamplerState State;
+    public readonly ID3D11SamplerState* State;
     
-    public D3D11SamplerState(ID3D11Device device, SamplerStateDescription description)
+    public D3D11SamplerState(ID3D11Device* device, SamplerStateDescription description)
     {
         Description = description;
         
-        Filter filter = description.Filter switch
+        D3D11_FILTER filter = description.Filter switch
         {
-            TextureFilter.Anisotropic => Filter.Anisotropic,
-            TextureFilter.MinMagMipPoint => Filter.MinMagMipPoint,
-            TextureFilter.MinMagPointMipLinear => Filter.MinMagPointMipLinear,
-            TextureFilter.MinPointMagLinearMipPoint => Filter.MinPointMagLinearMipPoint,
-            TextureFilter.MinPointMagMipLinear => Filter.MinPointMagMipLinear,
-            TextureFilter.MinLinearMagMipPoint => Filter.MinLinearMagMipPoint,
-            TextureFilter.MinLinearMagPointMipLinear => Filter.MinLinearMagPointMipLinear,
-            TextureFilter.MinMagLinearMipPoint => Filter.MinMagLinearMipPoint,
-            TextureFilter.MinMagMipLinear => Filter.MinMagMipLinear,
+            TextureFilter.Anisotropic => D3D11_FILTER.D3D11_FILTER_ANISOTROPIC,
+            TextureFilter.MinMagMipPoint => D3D11_FILTER.D3D11_FILTER_MIN_MAG_MIP_POINT,
+            TextureFilter.MinMagPointMipLinear => D3D11_FILTER.D3D11_FILTER_MIN_MAG_POINT_MIP_LINEAR,
+            TextureFilter.MinPointMagLinearMipPoint => D3D11_FILTER.D3D11_FILTER_MIN_POINT_MAG_LINEAR_MIP_POINT,
+            TextureFilter.MinPointMagMipLinear => D3D11_FILTER.D3D11_FILTER_MIN_POINT_MAG_MIP_LINEAR,
+            TextureFilter.MinLinearMagMipPoint => D3D11_FILTER.D3D11_FILTER_MIN_LINEAR_MAG_MIP_POINT,
+            TextureFilter.MinLinearMagPointMipLinear => D3D11_FILTER.D3D11_FILTER_MIN_LINEAR_MAG_POINT_MIP_LINEAR,
+            TextureFilter.MinMagLinearMipPoint => D3D11_FILTER.D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT,
+            TextureFilter.MinMagMipLinear => D3D11_FILTER.D3D11_FILTER_MIN_MAG_MIP_LINEAR,
             _ => throw new ArgumentOutOfRangeException()
         };
-
-        SamplerDescription desc = new SamplerDescription()
+        
+        D3D11_SAMPLER_DESC desc = new()
         {
             Filter = filter,
             AddressU = GetAddressModeFromTextureAddress(description.AddressU),
             AddressV = GetAddressModeFromTextureAddress(description.AddressV),
             AddressW = GetAddressModeFromTextureAddress(description.AddressW),
             MipLODBias = 0,
-            MaxAnisotropy = description.MaxAnisotropy,
-            ComparisonFunc = Vortice.Direct3D11.ComparisonFunction.LessEqual,
+            MaxAnisotropy = (uint) description.MaxAnisotropy,
+            ComparisonFunc = D3D11_COMPARISON_LESS_EQUAL,
             MinLOD = description.MinLOD,
-            MaxLOD = description.MaxLOD,
-            BorderColor = new Color4(description.BorderColor.R, description.BorderColor.G, description.BorderColor.B, description.BorderColor.A)
+            MaxLOD = description.MaxLOD
         };
 
-        State = device.CreateSamplerState(desc);
+        // TODO: ???????????????????????????
+        desc.BorderColor[0] = description.BorderColor.R / 255f;
+        desc.BorderColor[1] = description.BorderColor.G / 255f;
+        desc.BorderColor[2] = description.BorderColor.B / 255f;
+        desc.BorderColor[3] = description.BorderColor.A / 255f;
+
+        ID3D11SamplerState* state;
+        if (Failed(device->CreateSamplerState(&desc, &state)))
+            throw new PieException("Failed to create sampler state.");
+
+        State = state;
     }
     
     public override bool IsDisposed { get; protected set; }
@@ -52,17 +65,17 @@ internal sealed class D3D11SamplerState : SamplerState
         if (IsDisposed)
             return;
         IsDisposed = true;
-        State.Dispose();
+        State->Release();
     }
 
-    private static TextureAddressMode GetAddressModeFromTextureAddress(TextureAddress address)
+    private static D3D11_TEXTURE_ADDRESS_MODE GetAddressModeFromTextureAddress(TextureAddress address)
     {
         return address switch
         {
-            TextureAddress.Repeat => TextureAddressMode.Wrap,
-            TextureAddress.Mirror => TextureAddressMode.Mirror,
-            TextureAddress.ClampToEdge => TextureAddressMode.Clamp,
-            TextureAddress.ClampToBorder => TextureAddressMode.Border,
+            TextureAddress.Repeat => D3D11_TEXTURE_ADDRESS_WRAP,
+            TextureAddress.Mirror => D3D11_TEXTURE_ADDRESS_MIRROR,
+            TextureAddress.ClampToEdge => D3D11_TEXTURE_ADDRESS_CLAMP,
+            TextureAddress.ClampToBorder => D3D11_TEXTURE_ADDRESS_BORDER,
             _ => throw new ArgumentOutOfRangeException(nameof(address), address, null)
         };
     }
